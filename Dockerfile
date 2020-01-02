2dd49c065c84b281208242a097d4dc40e4190f59
Mircea (Mark) Halmagiu <mircea@smartly.io>
2020-01-02 13:28:58 +0200

    First test of functionality

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-# Container image that runs your code
 FROM alpine:3.10
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
+RUN apk add jq curl
+
 COPY entrypoint.sh /entrypoint.sh
 
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+PAYLOAD=$(echo '{}' | jq --arg ref "$GITHUB_HEAD_REF" '.ref = $ref' | jq '.transient_environment = true' | jq '.production_environment = false' | jq '.auto_merge = false' | jq '.required_contexts = []' )
+DEPLOYMENTS_URL="https://api.github.com/repos/$GITHUB_REPOSITORY/deployments"
+echo "$PAYLOAD"
+echo "$DEPLOYMENTS_URL"
+DEPLOYMENT=$(curl --fail -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.ant-man-preview+json" --data "$PAYLOAD" "$DEPLOYMENTS_URL")
+echo $DEPLOYMENT
+STATUS_PAYLOAD=$(echo '{}' | jq --arg environment_url "$ENVIRONMENT_URL" '.environment_url = $environment_url' | jq '.state = "success"' )
+DEPLOYMENT_URL=$(echo $DEPLOYMENT | jq -r .url)
+echo "$STATUS_PAYLOAD"
+echo "$DEPLOYMENT_URL/statuses"
+curl --fail -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.ant-man-preview+json" --data "$STATUS_PAYLOAD" "$DEPLOYMENT_URL/statuses" > /dev/null
