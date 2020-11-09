22ecdc2e871293867950924081616cbad70a71df
Simon Jagoe <simon.jagoe@smartly.io>
2020-11-09 12:29:39 +0200

    Implement context loading with basic tests

---
+++ __tests__/context.test.ts
@@ -0,0 +1,172 @@
+import {
+  Context,
+  Environment,
+  getContext,
+  saveExecutionState,
+  executionStage,
+  PRE_HAS_RUN,
+  MAIN_HAS_RUN,
+  DEPLOYMENT_ID
+} from '../src/context'
+import {mocked} from 'ts-jest/utils';
+import {getInput, saveState, getState, info} from '@actions/core';
+
+jest.mock('@actions/core', () => ({
+  getInput: jest.fn(),
+  saveState: jest.fn(),
+  getState: jest.fn(),
+  info: jest.fn()
+}));
+
+const OLD_ENV = process.env;
+beforeEach(() => {
+  process.env = {...OLD_ENV};
+});
+
+afterEach(() => {
+  process.env = OLD_ENV;
+});
+
+describe('executionStage', () => {
+  test('return pre if no stages have completed yet', () => {
+    delete process.env[`STATE_${PRE_HAS_RUN}`]
+    delete process.env[`STATE_${MAIN_HAS_RUN}`]
+    expect(executionStage()).toEqual('pre')
+  })
+
+  test('return main if only the pre stage has completed', () => {
+    process.env[`STATE_${PRE_HAS_RUN}`] = 'true'
+    delete process.env[`STATE_${MAIN_HAS_RUN}`]
+    expect(executionStage()).toEqual('main')
+  })
+
+  test('return post if the main stage has completed', () => {
+    process.env[`STATE_${PRE_HAS_RUN}`] = 'true'
+    process.env[`STATE_${MAIN_HAS_RUN}`] = 'true'
+    expect(executionStage()).toEqual('post')
+  })
+
+  test('return post if only the main stage has completed', () => {
+    delete process.env[`STATE_${PRE_HAS_RUN}`]
+    process.env[`STATE_${MAIN_HAS_RUN}`] = 'true'
+    expect(executionStage()).toEqual('post')
+  })
+})
+
+describe('saveExecutionState', () => {
+  const context: Context = {
+    executionStage: 'pre',
+    token: '',
+    repoOwner: '',
+    repoName: '',
+    ref: '',
+    version: '',
+    requiredContexts: [],
+    jobStatus: '',
+    environment: {
+      name: '',
+      url: '',
+      isProduction: false,
+      isTransient: true
+    },
+  }
+  it('should save the execution stage in pre execution', async () => {
+    const mockSaveState = mocked(saveState)
+    saveExecutionState(context)
+    expect(mockSaveState).toHaveBeenCalledTimes(1)
+    expect(mockSaveState).toHaveBeenCalledWith(PRE_HAS_RUN, 'true')
+  })
+
+  it('should save the execution stage in main execution', async () => {
+    const mockSaveState = mocked(saveState)
+    context.executionStage = 'main'
+    saveExecutionState(context)
+    expect(mockSaveState).toHaveBeenCalledTimes(2)
+    expect(mockSaveState).toHaveBeenCalledWith(PRE_HAS_RUN, 'true')
+    expect(mockSaveState).toHaveBeenCalledWith(MAIN_HAS_RUN, 'true')
+  })
+
+  it('should save the execution stage in post execution', async () => {
+    const mockSaveState = mocked(saveState)
+    context.executionStage = 'post'
+    saveExecutionState(context)
+    expect(mockSaveState).toHaveBeenCalledTimes(2)
+    expect(mockSaveState).toHaveBeenCalledWith(PRE_HAS_RUN, 'true')
+    expect(mockSaveState).toHaveBeenCalledWith(MAIN_HAS_RUN, 'true')
+  })
+
+  it('should save deployment ID', async () => {
+    const mockSaveState = mocked(saveState)
+    context.executionStage = 'post'
+    context.deploymentId = 7
+    saveExecutionState(context)
+    expect(mockSaveState).toHaveBeenCalledTimes(3)
+    expect(mockSaveState).toHaveBeenCalledWith(PRE_HAS_RUN, 'true')
+    expect(mockSaveState).toHaveBeenCalledWith(MAIN_HAS_RUN, 'true')
+    expect(mockSaveState).toHaveBeenCalledWith(DEPLOYMENT_ID, '7')
+  })
+})
+
+describe('context', () => {
+  delete process.env[`STATE_${PRE_HAS_RUN}`]
+  delete process.env[`STATE_${MAIN_HAS_RUN}`]
+  delete process.env[`STATE_${DEPLOYMENT_ID}`]
+  process.env[`GITHUB_REPOSITORY`] = 'smartlyio/ci-sla'
+  process.env[`GITHUB_REF`] = 'refs/heads/master'
+
+  test('no repository inputs', async () => {
+    delete process.env[`GITHUB_REPOSITORY`]
+    await expect(getContext()).rejects.toThrow(/Unexpectedly missing.*GITHUB_REPOSITORY/)
+  })
+
+  test('no ref inputs', async () => {
+    delete process.env[`GITHUB_REF`]
+    await expect(getContext()).rejects.toThrow(/No 'ref' input provided/)
+  })
+
+  test('basic context construction with all inputs', async () => {
+    const mockGetInput = mocked(getInput)
+    const ref = 'refs/heads/master'
+    const version = 'v0.12.4'
+    const environment = 'production'
+    const environmentUrl = 'envurl'
+    const token = 'token'
+    const jobStatus = 'success'
+    mockGetInput.mockImplementation((name: string): string => {
+      switch (name) {
+        case 'ref':
+          return ref
+        case 'version':
+          return version
+        case 'required_contexts':
+          return ''
+        case 'environment_name':
+          return environment
+        case 'environment_url':
+          return environmentUrl
+        case 'github_token':
+          return token
+        case 'job_status':
+          return jobStatus
+        default:
+          return ''
+      }
+    })
+
+    const context = await getContext()
+    expect(context).toEqual(expect.objectContaining({
+      executionStage: 'pre',
+      token,
+      environment: expect.objectContaining({
+        name: environment,
+        url: environmentUrl,
+        isProduction: true,
+        isTransient: false
+      }),
+      requiredContexts: [],
+      ref,
+      version,
+      jobStatus
+    }))
+  })
+})
--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,35 @@
 import * as core from '@actions/core'
 
-interface Environment {
+export const PRE_HAS_RUN = 'preHasRun'
+export const MAIN_HAS_RUN = 'mainHasRun'
+export const DEPLOYMENT_ID = 'deploymentId'
+
+export function executionStage(): string {
+  const isMain = !!process.env[`STATE_${PRE_HAS_RUN}`]
+  const isPost = !!process.env[`STATE_${MAIN_HAS_RUN}`]
+  if (!isMain && !isPost) {
+    return 'pre'
+  } else if (isMain && !isPost) {
+    return 'main'
+  } else if (isPost) {
+    return 'post'
+  }
+  throw new Error('It should not be possible to reach this')
+}
+
+export function saveExecutionState(context: Context): void {
+  if (['pre', 'main', 'post'].includes(context.executionStage)) {
+    core.saveState(PRE_HAS_RUN, 'true')
+  }
+  if (['main', 'post'].includes(context.executionStage)) {
+    core.saveState(MAIN_HAS_RUN, 'true')
+  }
+  if (context.deploymentId) {
+    core.saveState(DEPLOYMENT_ID, JSON.stringify(context.deploymentId))
+  }
+}
+
+export interface Environment {
   name: string
   isProduction: boolean
   isTransient: boolean
@@ -8,13 +37,15 @@ interface Environment {
 }
 
 export interface Context {
+  executionStage: string
   token: string
+  jobStatus: string
   environment: Environment
   repoOwner: string
   repoName: string
   ref: string
-  version: string
-  requiredContexts: string[]
+  version?: string
+  requiredContexts: string[] | null
   deploymentId?: number
 }
 
@@ -31,30 +62,55 @@ export function parseArray(value: string): string[] {
 }
 
 export async function getContext(): Promise<Context> {
+  const stage = executionStage()
+
+  const githubRepository: string | undefined = process.env['GITHUB_REPOSITORY']
+  if (!githubRepository) {
+    throw new Error('Unexpectedly missing Github context GITHUB_REPOSITORY!')
+  }
+  const [repoOwner, repoName] = githubRepository.split('/')
+
+  const ref: string | undefined =
+    core.getInput('ref') || process.env['GITHUB_REF']
+  if (!ref) {
+    throw new Error(
+      "No 'ref' input provided and GITHUB_REF not available in the environment!"
+    )
+  }
+
+  const version: string = core.getInput('version')
+  const deploymentId = core.getState('deploymentId')
+  const requiredContexts = core.getInput('required_contexts') || ''
+
   const environmentName: string = core.getInput('environment_name')
-  const isProduction: boolean = environment === 'production'
+  const isProduction: boolean = environmentName === 'production'
   const environmentUrl: string = core.getInput('environment_url')
   const environment: Environment = {
     name: environmentName,
-    url: environmentUrl,
     isProduction,
     isTransient: !isProduction
   }
-
-  const ref: string = core.getInput('ref')
-  const version: string = core.getInput('version')
-
-  const deploymentId = core.getState('deploymentId')
+  if (environmentUrl) {
+    environment.url = environmentUrl
+  }
 
   const context: Context = {
-    token: core.getInput('github_token'),
-    requiredContexts: parseArray(core.getInput('required_contexts')),
+    executionStage: stage,
+    token: core.getInput('github_token', {required: true}),
+    jobStatus: core.getInput('job_status', {required: true}),
+    requiredContexts: parseArray(requiredContexts),
+    repoOwner,
+    repoName,
     ref,
-    version,
     environment
+  }
+  if (version) {
+    context.version = version
   }
   if (deploymentId) {
     context.deploymentId = parseInt(deploymentId)
   }
+
+  saveExecutionState(context)
   return context
 }
