bddd695bc0b88b0fcb8d7bb7cb45f2e21af71945
Simon Jagoe <simon.jagoe@smartly.io>
2020-11-10 12:50:56 +0200

    Use separate entrypoints for pre/main/post stages for resilience

--- src/post.ts
+++ src/post.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
 import {Context, getContext, saveExecutionState} from './context'
-import {setDeploymentInProgress} from './deployment'
+import {setDeploymentEnded} from './deployment'
 
 async function run(): Promise<void> {
   try {
     const context: Context = await getContext()
-    if (context.executionStage !== 'main') {
-      throw new Error(
-        `Unexpected execution stage "${context.executionStage}" when executing main stage`
-      )
+    if (context.executionStage !== 'post') {
+      // This will happen if the 'main' stage was skipped, e.g. due to
+      // an `if:` condition in the workflow.
+      context.jobStatus = 'error'
     }
-    await setDeploymentInProgress(context)
+    await setDeploymentEnded(context)
     saveExecutionState(context)
   } catch (error) {
     core.setFailed(error.message)
--- src/pre.ts
+++ src/pre.ts
@@ -1,16 +1,17 @@
 import * as core from '@actions/core'
 import {Context, getContext, saveExecutionState} from './context'
-import {setDeploymentInProgress} from './deployment'
+import {createDeployment} from './deployment'
 
 async function run(): Promise<void> {
   try {
     const context: Context = await getContext()
-    if (context.executionStage !== 'main') {
+    if (context.executionStage !== 'pre') {
+      // This could happen if there is an error creating the deployment, and state is not saved.
       throw new Error(
-        `Unexpected execution stage "${context.executionStage}" when executing main stage`
+        `Unexpected execution stage "${context.executionStage}" when executing pre stage.`
       )
     }
-    await setDeploymentInProgress(context)
+    await createDeployment(context)
     saveExecutionState(context)
   } catch (error) {
     core.setFailed(error.message)
